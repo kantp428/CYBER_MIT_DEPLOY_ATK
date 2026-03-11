1582d842b6cd8263860fc7848b53eadaa18c658f
mrktp <misterkantapat@gmail.com>
2026-03-11 14:21:25 +0700

    Initial commit (fresh start)

---
+++ smart-air/app/layout.tsx
@@ -0,0 +1,29 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Smart-Air (MIT-Project)",
+  description: "Generate By Chiraphat Kongkiat Kantapat Supakorn",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className="antialiased">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
