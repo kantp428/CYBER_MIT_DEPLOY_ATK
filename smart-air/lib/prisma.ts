1291c0e3abd21690cef2b46ef261e7c2b73fc8f5
Trai2548 <22trai2546@gmail.com>
2026-03-11 18:05:34 +0700

    Merge remote-tracking branch 'origin/dev' into test2

--- smart-air/lib/prisma.ts
+++ smart-air/lib/prisma.ts
@@ -1,10 +1,8 @@
-import { Pool } from "pg";
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
 const createPrismaClient = () => {
-  const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-  const adapter = new PrismaPg(pool);
+  const adapter = new PrismaMariaDb(process.env.DATABASE_URL!);
 
   return new PrismaClient({
     adapter,
